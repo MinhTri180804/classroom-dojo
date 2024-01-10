f7b9f80d0c3bac6a06730ccfd4fda2be416d2c99
ShiroganeIXV <wudiana.124010121105@vtc.edu.vn>
2024-01-10 14:50:09 +0700

    upload final

--- src/apis/axiosClient.jsx
+++ src/apis/axiosClient.jsx
@@ -2,7 +2,9 @@ import axios from "axios";
 import queryString from "query-string";
 
 const axiosClient = axios.create({
-  baseURL: "https://ec2-18-142-251-214.ap-southeast-1.compute.amazonaws.com",
+  // baseURL: "https://ec2-18-142-251-214.ap-southeast-1.compute.amazonaws.com",
+  baseURL: "http://18.142.251.214:8080",
+  
   headers: {
     "Content-Type": "application/json",
   },
